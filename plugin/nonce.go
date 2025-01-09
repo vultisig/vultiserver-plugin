38253877e542d23a3f7ccf02236c7e5dc11f727e
Toufic <touficbatrice@gmail.com>
2025-01-09 18:22:43 +0100

    fetch nonce, and check status of tx

--- plugin/nonce.go
+++ plugin/nonce.go
@@ -25,20 +25,10 @@ func (n *NonceManager) GetNextNonce(address string) (uint64, error) {
 	n.mu.Lock()
 	defer n.mu.Unlock()
 
-	// Try to get from local cache
-	if nonce, ok := n.nonceMap.Load(address); ok {
-		nextNonce := nonce.(uint64) + 1
-		n.nonceMap.Store(address, nextNonce)
-		return nextNonce, nil
-	}
-
-	// Get from network
 	nonce, err := n.rpcClient.PendingNonceAt(context.Background(), common.HexToAddress(address))
 	if err != nil {
 		return 0, fmt.Errorf("failed to get nonce from network: %w", err)
 	}
-
-	n.nonceMap.Store(address, nonce)
 	return nonce, nil
 }
 
--- plugin/payroll/payroll.go
+++ plugin/payroll/payroll.go
@@ -8,14 +8,18 @@ import (
 	"fmt"
 	"math/big"
 	"strings"
+	"time"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	gcommon "github.com/ethereum/go-ethereum/common"
 	gtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/google/uuid"
+	"github.com/klaytn/klaytn/common"
 	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
 	"github.com/vultisig/vultisigner/internal/types"
 	"github.com/vultisig/vultisigner/plugin"
 	"github.com/vultisig/vultisigner/storage"
@@ -39,6 +43,7 @@ type PayrollPlugin struct {
 	db           storage.DatabaseStorage
 	nonceManager *plugin.NonceManager
 	rpcClient    *ethclient.Client
+	logger       *logrus.Logger
 }
 
 func NewPayrollPlugin(db storage.DatabaseStorage, rpcClient *ethclient.Client) *PayrollPlugin {
@@ -46,6 +51,7 @@ func NewPayrollPlugin(db storage.DatabaseStorage, rpcClient *ethclient.Client) *
 		db:           db,
 		rpcClient:    rpcClient,
 		nonceManager: plugin.NewNonceManager(rpcClient),
+		logger:       logrus.New(),
 	}
 }
 
@@ -254,7 +260,7 @@ func (p *PayrollPlugin) GetNextNonce(address string) (uint64, error) {
 	return p.nonceManager.GetNextNonce(address)
 }
 
-func (p *PayrollPlugin) SigningComplete(signedTx types.SignedTransaction) error {
+/*func (p *PayrollPlugin) SigningComplete(signedTx types.SignedTransaction) error {
 	// implement broadcast logic
 	tx := new(gtypes.Transaction)
 	if err := tx.UnmarshalBinary([]byte(signedTx.RawTx)); err != nil {
@@ -279,4 +285,148 @@ func (p *PayrollPlugin) SigningComplete(signedTx types.SignedTransaction) error 
 	}
 
 	return nil
+}*/
+
+func (p *PayrollPlugin) SigningComplete(signedTx types.SignedTransaction) error {
+	tx := new(gtypes.Transaction)
+	if err := tx.UnmarshalBinary([]byte(signedTx.RawTx)); err != nil {
+		return fmt.Errorf("failed to unmarshal transaction: %w", err)
+	}
+
+	// get sender for logging
+	signer := gtypes.NewLondonSigner(tx.ChainId())
+	sender, err := signer.Sender(tx)
+	if err != nil {
+		p.logger.Warnf("Could not determine sender: %v", err)
+	}
+
+	// send tx
+	err = p.rpcClient.SendTransaction(context.Background(), tx)
+	if err != nil {
+		return p.handleTransactionError(err, tx, sender) //Todo:the worker gets back the tx error, and acts accordingly
+	}
+
+	return p.monitorTransaction(tx)
+}
+
+func (p *PayrollPlugin) handleTransactionError(err error, tx *gtypes.Transaction, sender common.Address) error {
+	errMsg := err.Error()
+
+	switch {
+	case strings.Contains(errMsg, "nonce too low"): //Todo : check that the error messages are correct
+		return &types.TransactionError{
+			Code:    types.ErrNonce,
+			Message: fmt.Sprintf("Transaction with nonce %d already exists. Retry with new nonce", tx.Nonce()),
+			Err:     err,
+		}
+
+	case strings.Contains(errMsg, "nonce too high"):
+		return &types.TransactionError{
+			Code:    types.ErrNonce,
+			Message: "Gap in nonces detected. Previous transaction might be pending",
+			Err:     err,
+		}
+
+	case strings.Contains(errMsg, "insufficient funds"):
+		return &types.TransactionError{
+			Code:    types.ErrInsufficientFunds,
+			Message: fmt.Sprintf("Account %s has insufficient funds", sender.Hex()),
+			Err:     err,
+		}
+
+	case strings.Contains(errMsg, "gas price too low"):
+		return &types.TransactionError{
+			Code:    types.ErrGasPriceUnderpriced,
+			Message: fmt.Sprintf("Current gas price %s is too low", tx.GasPrice().String()),
+			Err:     err,
+		}
+
+	case strings.Contains(errMsg, "gas limit reached"):
+		return &types.TransactionError{
+			Code:    types.ErrGasTooLow,
+			Message: fmt.Sprintf("Gas limit %d is too low", tx.Gas()),
+			Err:     err,
+		}
+
+	default:
+		return &types.TransactionError{
+			Code:    types.ErrRPCConnectionFailed,
+			Message: "Unknown RPC error",
+			Err:     err,
+		}
+	}
+}
+
+func (p *PayrollPlugin) monitorTransaction(tx *gtypes.Transaction) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute) //how much time should we monitor the tx?
+	defer cancel()
+
+	ticker := time.NewTicker(15 * time.Second)
+	defer ticker.Stop()
+
+	txHash := tx.Hash()
+	for {
+		select {
+		case <-ctx.Done():
+			return &types.TransactionError{
+				Code:    types.ErrTxTimeout,
+				Message: fmt.Sprintf("Transaction monitoring timed out for tx: %s", txHash.Hex()),
+			}
+
+		case <-ticker.C:
+			// check tx status
+			_, isPending, err := p.rpcClient.TransactionByHash(ctx, txHash)
+			if err != nil {
+				if err == ethereum.NotFound {
+					return &types.TransactionError{
+						Code:    types.ErrTxDropped,
+						Message: fmt.Sprintf("Transaction dropped from mempool: %s", txHash.Hex()),
+					}
+				}
+				continue // keep trying on other RPC errors
+			}
+
+			if !isPending {
+				receipt, err := p.rpcClient.TransactionReceipt(ctx, txHash)
+				if err != nil {
+					continue
+				}
+
+				if receipt.Status == 0 {
+					// try to get revert reason
+					reason := p.getRevertReason(ctx, tx, receipt.BlockNumber)
+					return &types.TransactionError{
+						Code:    types.ErrExecutionReverted,
+						Message: fmt.Sprintf("Transaction reverted: %s", reason),
+					}
+				}
+
+				// Transaction successful
+				return nil
+			}
+		}
+	}
+}
+
+func (p *PayrollPlugin) getRevertReason(ctx context.Context, tx *gtypes.Transaction, blockNum *big.Int) string {
+	callMsg := ethereum.CallMsg{
+		To:       tx.To(),
+		Data:     tx.Data(),
+		Gas:      tx.Gas(),
+		GasPrice: tx.GasPrice(),
+		Value:    tx.Value(),
+	}
+
+	_, err := p.rpcClient.CallContract(ctx, callMsg, blockNum)
+	if err != nil {
+		// try to parse standard revert reason
+		if strings.Contains(err.Error(), "execution reverted:") {
+			parts := strings.Split(err.Error(), "execution reverted:")
+			if len(parts) > 1 {
+				return strings.TrimSpace(parts[1])
+			}
+		}
+		return err.Error()
+	}
+	return "Unknown revert reason"
 }
