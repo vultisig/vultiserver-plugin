8eaaecbc4e6ecabeb9cdff4359f12c2b9c25e151
Misho <mihail.genchev@limechain.tech>
2025-04-15 13:18:36 +0300

    refactored FrontEnd schema method

--- plugin/dca/dca.go
+++ plugin/dca/dca.go
@@ -42,6 +42,11 @@ const (
 )
 
 var (
+	//go:embed dcaPluginUiSchema.json
+	embeddedFileDCASchema embed.FS
+)
+
+var (
 	ErrCompletedPolicy = errors.New("policy completed all swaps")
 )
 
@@ -681,8 +686,8 @@ func (p *DCAPlugin) getApproveABI() (abi.ABI, error) {
 	return abi.JSON(strings.NewReader(approveABI))
 }
 
-func (p *DCAPlugin) FrontendSchema() embed.FS {
-	return embed.FS{}
+func (p *DCAPlugin) FrontendSchema() ([]byte, error) {
+	return embeddedFileDCASchema.ReadFile("dcaPluginUiSchema.json")
 }
 
 type RawTxData struct {
